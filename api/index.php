caa315f2a80c74da073a00c9cdd752e1e9f3072f
George Webb <george@gawdev.co.uk>
2016-05-25 21:51:55 +0100

    Implement monolog logging

--- api/index.php
+++ api/index.php
@@ -11,14 +11,29 @@ require_once dirname(__DIR__) . '/config/bootstrap.php';
 use Slim\App;
 use Slim\Container;
 use TrkLife\Config;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
+// Slim configuration
 $slim_conf = array(
     'settings' => array(
         'displayErrorDetails' => Config::get('DisplayErrors'),
     ),
 );
 
+// DI Container
 $c = new Container($slim_conf);
+
+// Add logging to DI Container
+$c['logger'] = function() {
+    $logger = new Logger('trk.life');
+    $logger->pushHandler(new StreamHandler(
+        Config::get('RootDir') . "/tmp/logs/app.log",
+        Logger::toMonologLevel(Config::get('LogLevel'))
+    ));
+    return $logger;
+};
+
 $app = new App($c);
 
 /**
