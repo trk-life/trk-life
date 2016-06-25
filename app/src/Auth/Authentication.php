c6b91770a79b61c59722ba39e875dad044faa9f0
George Webb <george@gawdev.co.uk>
2016-06-25 22:01:58 +0800

    Check that user actually exists for token on authentication

--- app/src/Auth/Authentication.php
+++ app/src/Auth/Authentication.php
@@ -51,7 +51,14 @@ class Authentication
 
         // Set user and token in request attribute
         $user_repository = $this->c->EntityManager->getRepository('TrkLife\Entity\User');
-        $request = $request->withAttribute('user', $user_repository->findOneById($token_entity->get('user_id')));
+        $user = $user_repository->findOneById($token_entity->get('user_id'));
+
+        // Check user exists
+        if ($user === null) {
+            return false;
+        }
+
+        $request = $request->withAttribute('user', $user);
         $request = $request->withAttribute('token_entity', $token_entity);
 
         return $request;
